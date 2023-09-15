caef102c381627ca553b8154e4da3085b0f42d31
JoaoMaria Vizoso (ext.) <JoaoMaria.Vizoso@partner.bmw.de>
2023-09-15 10:13:28 +0100

    First commit

---
+++ org/prd/main.tf
@@ -0,0 +1,29 @@
+# Specify the provider and credentials for GCP
+provider "google" {
+  project     = "dynamic-circle-398514"
+  region      = "us-central1"
+}
+
+terraform {
+  backend "gcs" {
+    bucket  = "tf-states-pv-prd"
+    prefix  = "terraform/state-prd"
+  }
+}
+
+
+# Create a Google Cloud Storage bucket
+resource "google_storage_bucket" "example_bucket" {
+  name          = "bucket-110920231244-prd"
+  location      = "US" 
+}
+
+
+
+
+
+
+
+
+
+
